b222eb26a148fd3f8814d5df701ecd4b0e9fea67
Hanna <hannaeberhoefer@yahoo.com>
2021-12-21 19:44:10 +0100

    feat: add middleware

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import reducer from './reducers';
+import middleware from './middleware';
 
-const store = createStore(reducer)
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
