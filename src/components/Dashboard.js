7f80bcea0aa59c189cdb403c46a58a459ece9376
Hanna <hannaeberhoefer@yahoo.com>
2021-12-26 17:41:11 +0100

    feat: Dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Dashboard extends Component {
     render() {
+
+        const { authedUser } = this.props;
+
+        const answeredQuestions = this.props.questions.filter(
+                question => question.optionOne.votes.includes(authedUser) 
+                || question.optionTwo.votes.includes(authedUser))
+
+        const unansweredQuestions = this.props.questions.filter(
+                question => !question.optionOne.votes.includes(authedUser)
+                && !question.optionTwo.votes.includes(authedUser))            
+                    
         return (
-            <h1>Dashboard</h1>
+            <div>
+                <div>
+                    <h3>Answered Polls</h3>
+                    <ul>
+                        {answeredQuestions.map(question => (
+                            <li key={question.id}>
+                                <div>{question.id}</div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <h3>Unanswered Polls</h3>
+                    <ul>
+                    {unansweredQuestions.map(question => (
+                            <li key={question.id}>
+                                <div>{question.id}</div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         )
     }
 }
 
-export default Dashboard;
+function mapStateToProps({ authedUser, questions }) {
+    return {
+        authedUser,
+        questions: Object.values(questions).sort((a, b) => 
+            b.timestamp - a.timestamp)
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard);
