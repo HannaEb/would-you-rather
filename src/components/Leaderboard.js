47e3f81de3e3967c66725aa2277b333a345e4020
Hanna <hannaeberhoefer@yahoo.com>
2021-12-28 13:16:34 +0100

    feat: display users

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Leaderboard extends Component {
     render() {
+
+        const  { users }  = this.props
+
         return (
-            <h1>Leaderboard</h1>
+            <div>
+                <ul>
+                    {users.map(user => {
+                        const answeredQuestions = Object.keys(user.answers).length
+                        const createdQuestions = user.questions.length
+                        const score = answeredQuestions + createdQuestions
+                        return (
+                            <li key={user.id}>
+                                <h4>{user.name}</h4>
+                                <p>Answered questions: {answeredQuestions}</p>
+                                <p>Created questions: {createdQuestions}</p>
+                                <p>Score: {score}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
         )
     }
 }
 
-export default Leaderboard;
+function mapStateToProps({ users }) {
+    return {
+        users: Object.values(users)
+    }
+}
+
+export default connect(mapStateToProps)(Leaderboard);
