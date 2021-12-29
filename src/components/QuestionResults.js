e62a6b1df3e312ab185c4f01123881072dcf5cf5
Hanna <hannaeberhoefer@yahoo.com>
2021-12-29 18:24:29 +0100

    feat: authedUser's choice

--- src/components/QuestionResults.js
+++ src/components/QuestionResults.js
@@ -5,11 +5,13 @@ import { calculatePercentage } from '../utils/helpers';
 class QuestionResults extends Component {
     render(){
 
-        const { question } = this.props
+        const { question, authedUser } = this.props
         const { author, optionOne, optionTwo } = question
         const optionOneVotes = (optionOne.votes).length
         const optionTwoVotes = (optionTwo.votes).length
         const totalVotes = optionOneVotes + optionTwoVotes
+        const optionOneChoice = (optionOne.votes).includes(authedUser)
+        const optionTwoChoice = (optionTwo.votes).includes(authedUser)
         
         return (
             <div>
@@ -19,22 +21,29 @@ class QuestionResults extends Component {
                     <h4>Would you rather {optionOne.text}?</h4>
                     <p>{calculatePercentage(optionOneVotes, totalVotes)}%</p>
                     <p>{optionOneVotes} out of {totalVotes} votes</p>
+                    {optionOneChoice && (
+                        <p>You chose this option</p>
+                    )}
                 </div>
                 <div>
                     <h4>Would you rather {optionTwo.text}?</h4>
                     <p>{calculatePercentage(optionTwoVotes, totalVotes)}%</p>
                     <p>{optionTwoVotes} out of {totalVotes} votes</p>
+                    {optionTwoChoice && (
+                        <p>You chose this option</p>
+                    )}
                 </div>
             </div>
         )
     }
 }
 
-function mapStateToProps({questions}, { id }) {
+function mapStateToProps({ questions, authedUser }, { id }) {
     const question = questions[id]
 
     return {
-        question
+        question, 
+        authedUser
     }
 }
 
