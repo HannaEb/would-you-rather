cc35ddf129b7f91aa918425dd6ea7a8f53b6ccae
Hanna <hannaeberhoefer@yahoo.com>
2022-01-01 18:02:47 +0100

    style: create tabs

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,42 +1,83 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Nav, NavItem, NavLink, TabContent, TabPane, Row, Col, List } from 'reactstrap';
 import Question from './Question';
+import classnames from 'classnames';
 
 class Dashboard extends Component {
+    state = {
+        activeTab: 'unanswered'
+    }
+
+    handleToggle = tab => {
+        const activeTab = this.state
+
+        if (activeTab !== tab) {
+            this.setState(() => ({
+                activeTab: tab
+            }))
+        }
+    }
+
     render() {
 
-        const { authedUser } = this.props;
+        const { authedUser } = this.props
+        const { activeTab } = this.state
+
+        const unansweredQuestions = this.props.questions.filter(
+            question => !question.optionOne.votes.includes(authedUser)
+            && !question.optionTwo.votes.includes(authedUser))     
 
         const answeredQuestions = this.props.questions.filter(
                 question => question.optionOne.votes.includes(authedUser) 
                 || question.optionTwo.votes.includes(authedUser))
-
-        const unansweredQuestions = this.props.questions.filter(
-                question => !question.optionOne.votes.includes(authedUser)
-                && !question.optionTwo.votes.includes(authedUser))            
-                    
+      
         return (
-            <div>
-                <div>
-                    <h3>Answered Polls</h3>
-                    <ul>
-                        {answeredQuestions.map(question => (
-                            <li key={question.id}>
-                                <Question id={question.id} />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div>
-                    <h3>Unanswered Polls</h3>
-                    <ul>
-                        {unansweredQuestions.map(question => (
-                            <li key={question.id}>
-                                <Question id={question.id} />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+            <div className = 'container'>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: activeTab === 'unanswered'})}
+                            onClick={() => this.handleToggle('unanswered')}>
+                                Unanswered Questions
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink
+                            className={classnames({ active: activeTab === 'answered'})}
+                            onClick={() => this.handleToggle('answered')}>
+                                Answered Questions
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={activeTab}>
+                    <TabPane tabId='unanswered'>
+                        <Row>
+                            <Col sm='6'>
+                                <List type='unstyled'>
+                                    {unansweredQuestions.map(question => (
+                                        <li key={question.id}>
+                                            <Question id={question.id} />
+                                        </li>
+                                    ))}
+                                </List>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId='answered'>
+                        <Row>
+                            <Col sm='12'>
+                                <List type='unstyled'>
+                                    {answeredQuestions.map(question => (
+                                        <li key={question.id}>
+                                            <Question id={question.id} />
+                                        </li>
+                                    ))}
+                                </List>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                </TabContent>
             </div>
         )
     }
