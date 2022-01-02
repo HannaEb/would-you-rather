abbb96505cd7e386940f8f466e8524e15bc58cda
Hanna <hannaeberhoefer@yahoo.com>
2022-01-02 14:39:12 +0100

    style: question cards

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -33,51 +33,55 @@ class Dashboard extends Component {
                 || question.optionTwo.votes.includes(authedUser))
       
         return (
-            <div className = 'container'>
-                <Nav tabs>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: activeTab === 'unanswered'})}
-                            onClick={() => this.handleToggle('unanswered')}>
-                                Unanswered Questions
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                    <NavLink
-                            className={classnames({ active: activeTab === 'answered'})}
-                            onClick={() => this.handleToggle('answered')}>
-                                Answered Questions
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-                <TabContent activeTab={activeTab}>
-                    <TabPane tabId='unanswered'>
-                        <Row>
-                            <Col sm='6'>
-                                <List type='unstyled'>
-                                    {unansweredQuestions.map(question => (
-                                        <li key={question.id}>
-                                            <Question id={question.id} />
-                                        </li>
-                                    ))}
-                                </List>
-                            </Col>
-                        </Row>
-                    </TabPane>
-                    <TabPane tabId='answered'>
-                        <Row>
-                            <Col sm='12'>
-                                <List type='unstyled'>
-                                    {answeredQuestions.map(question => (
-                                        <li key={question.id}>
-                                            <Question id={question.id} />
-                                        </li>
-                                    ))}
-                                </List>
-                            </Col>
-                        </Row>
-                    </TabPane>
-                </TabContent>
+            <div className='container'>
+                <Row className='justify-content-center'>
+                    <Col md='auto'>
+                        <Nav tabs>
+                            <NavItem>
+                                <NavLink
+                                    className={classnames({ active: activeTab === 'unanswered'})}
+                                    onClick={() => this.handleToggle('unanswered')}>
+                                        Unanswered Questions
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                            <NavLink
+                                    className={classnames({ active: activeTab === 'answered'})}
+                                    onClick={() => this.handleToggle('answered')}>
+                                        Answered Questions
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                        <TabContent activeTab={activeTab}>
+                            <TabPane tabId='unanswered'>
+                                <Row>
+                                    <Col>
+                                        <List type='unstyled'>
+                                            {unansweredQuestions.map(question => (
+                                                <li key={question.id}>
+                                                    <Question id={question.id} />
+                                                </li>
+                                            ))}
+                                        </List>
+                                    </Col>
+                                </Row>
+                            </TabPane>
+                            <TabPane tabId='answered'>
+                                <Row>
+                                    <Col>
+                                        <List type='unstyled'>
+                                            {answeredQuestions.map(question => (
+                                                <li key={question.id}>
+                                                    <Question id={question.id} />
+                                                </li>
+                                            ))}
+                                        </List>
+                                    </Col>
+                                </Row>
+                            </TabPane>
+                        </TabContent>
+                    </Col>
+                </Row>
             </div>
         )
     }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Card, CardHeader, CardImg, CardBody, CardTitle, CardText, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 class Question extends Component {
     render() {
 
-        const { question } = this.props
+        const { question, avatar } = this.props
         const { id, author, optionOne, optionTwo } = question
 
         return (
-            <Link to={`/questions/${id}`}>
-                <p>{author} asks:</p>
-                <h3>Would you rather...?</h3>
-                <p>{optionOne.text}</p>
-                <p>{optionTwo.text}</p>
-            </Link>
+            <Card className='mt-4'>
+                <CardHeader>{author} asks:</CardHeader>
+                <CardBody>
+                    <div className='row'>
+                        <div className='col'>
+                            <CardImg width='100%' className='d-block m-auto card-avatar' src={avatar} alt='Avatar'></CardImg>
+                        </div>
+                        <div className='col'>
+                            <CardTitle tag='h5'>Would you rather...</CardTitle>
+                            <CardText>...{optionOne.text.substring(0, 20)}...</CardText>
+                            <CardText>...{optionTwo.text.substring(0, 20)}...</CardText>
+                            <Link to={`/questions/${id}`}>
+                                <Button outline color='secondary'>View Details</Button>
+                            </Link>
+                        </div>
+                    </div>
+                </CardBody>
+            </Card>
         )
     }
 }
 
-function mapStateToProps({ questions }, { id }) {
+function mapStateToProps({ questions, users }, { id }) {
     const question = questions[id]
+    const avatar = users[question.author].avatarURL
+
     return {
+        avatar,
         question: question 
             ? question
             : null
