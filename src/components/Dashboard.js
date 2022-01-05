a2b420c11bc7699368b3c03b6eb8c0dfe14c03b6
Hanna <hannaeberhoefer@yahoo.com>
2022-01-05 13:09:52 +0100

    refactor: useState hook

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,90 +1,81 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Nav, NavItem, NavLink, TabContent, TabPane, Row, Col, List } from 'reactstrap';
 import Question from './Question';
 import classnames from 'classnames';
 
-class Dashboard extends Component {
-    state = {
-        activeTab: 'unanswered'
-    }
+const Dashboard = props => {
 
-    handleToggle = tab => {
-        const activeTab = this.state
+    const [activeTab, setActiveTab] = useState('unanswered')
 
+    const handleToggle = tab => {
         if (activeTab !== tab) {
-            this.setState(() => ({
-                activeTab: tab
-            }))
+            setActiveTab(tab)
         }
     }
 
-    render() {
+    const { authedUser, questions } = props
 
-        const { authedUser } = this.props
-        const { activeTab } = this.state
+    const unansweredQuestions = questions.filter(
+        question => !question.optionOne.votes.includes(authedUser)
+        && !question.optionTwo.votes.includes(authedUser))     
 
-        const unansweredQuestions = this.props.questions.filter(
-            question => !question.optionOne.votes.includes(authedUser)
-            && !question.optionTwo.votes.includes(authedUser))     
-
-        const answeredQuestions = this.props.questions.filter(
-                question => question.optionOne.votes.includes(authedUser) 
-                || question.optionTwo.votes.includes(authedUser))
-      
-        return (
-            <div className='container'>
-                <Row className='justify-content-center'>
-                    <Col md='auto'>
-                        <Nav tabs>
-                            <NavItem>
-                                <NavLink
-                                    className={classnames({ active: activeTab === 'unanswered'})}
-                                    onClick={() => this.handleToggle('unanswered')}>
-                                        Unanswered Questions
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
+    const answeredQuestions = questions.filter(
+            question => question.optionOne.votes.includes(authedUser) 
+            || question.optionTwo.votes.includes(authedUser))
+    
+    return (
+        <div className='container'>
+            <Row className='justify-content-center'>
+                <Col md='auto'>
+                    <Nav tabs>
+                        <NavItem>
                             <NavLink
-                                    className={classnames({ active: activeTab === 'answered'})}
-                                    onClick={() => this.handleToggle('answered')}>
-                                        Answered Questions
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                        <TabContent activeTab={activeTab}>
-                            <TabPane tabId='unanswered'>
-                                <Row>
-                                    <Col>
-                                        <List type='unstyled'>
-                                            {unansweredQuestions.map(question => (
-                                                <li key={question.id}>
-                                                    <Question id={question.id} />
-                                                </li>
-                                            ))}
-                                        </List>
-                                    </Col>
-                                </Row>
-                            </TabPane>
-                            <TabPane tabId='answered'>
-                                <Row>
-                                    <Col>
-                                        <List type='unstyled'>
-                                            {answeredQuestions.map(question => (
-                                                <li key={question.id}>
-                                                    <Question id={question.id} />
-                                                </li>
-                                            ))}
-                                        </List>
-                                    </Col>
-                                </Row>
-                            </TabPane>
-                        </TabContent>
-                    </Col>
-                </Row>
-            </div>
-        )
-    }
+                                className={classnames({ active: activeTab === 'unanswered'})}
+                                onClick={() => handleToggle('unanswered')}>
+                                    Unanswered Questions
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                        <NavLink
+                                className={classnames({ active: activeTab === 'answered'})}
+                                onClick={() => handleToggle('answered')}>
+                                    Answered Questions
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                    <TabContent activeTab={activeTab}>
+                        <TabPane tabId='unanswered'>
+                            <Row>
+                                <Col>
+                                    <List type='unstyled'>
+                                        {unansweredQuestions.map(question => (
+                                            <li key={question.id}>
+                                                <Question id={question.id} />
+                                            </li>
+                                        ))}
+                                    </List>
+                                </Col>
+                            </Row>
+                        </TabPane>
+                        <TabPane tabId='answered'>
+                            <Row>
+                                <Col>
+                                    <List type='unstyled'>
+                                        {answeredQuestions.map(question => (
+                                            <li key={question.id}>
+                                                <Question id={question.id} />
+                                            </li>
+                                        ))}
+                                    </List>
+                                </Col>
+                            </Row>
+                        </TabPane>
+                    </TabContent>
+                </Col>
+            </Row>
+        </div>
+    )
 }
 
 function mapStateToProps({ authedUser, questions }) {
