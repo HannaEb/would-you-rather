6700d51fcc1afbf06a927ed8c128fdfd5d9facd4
Hanna <hannaeberhoefer@yahoo.com>
2022-04-15 17:00:56 +0100

    feat: question model

---
+++ server/app/models/question.model.js
@@ -0,0 +1,30 @@
+module.exports = (mongoose) => {
+  const schema = mongoose.Schema(
+    {
+      timestamp: {
+        type: Date,
+        default: Date.now,
+      },
+      author: {
+        type: String,
+        required: true,
+      },
+      optionOne: {
+        votes: { type: Array },
+        text: { type: String, required: true },
+      },
+      optionTwo: {
+        votes: { type: Array },
+        text: { type: String, required: true },
+      },
+    }
+    // { timestamps: true }
+  );
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+  const Question = mongoose.model("question", schema);
+  return Question;
+};
