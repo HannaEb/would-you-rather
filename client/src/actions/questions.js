058fab10ec4f052622d0485a50249f10abbb6658
Hanna <hannaeberhoefer@yahoo.com>
2022-04-22 13:33:22 +0100

    refactor: question actions

--- client/src/actions/questions.js
+++ client/src/actions/questions.js
@@ -1,53 +1,121 @@
 import { saveQuestion, saveQuestionAnswer } from "../utils/api";
 import { updateUserQuestions, updateUserAnswers } from "./users";
+import QuestionDataService from "../services/question.service";
+import { useSelector } from "react-redux";
 
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
-export const ADD_QUESTION = "ADD_QUESTION";
+export const CREATE_QUESTION = "ADD_QUESTION";
 export const UPDATE_QUESTION = "UPDATE_QUESTION";
 
-export function receiveQuestions(questions) {
-  return {
-    type: RECEIVE_QUESTIONS,
-    questions,
-  };
-}
-
-function addQuestion(question) {
-  return {
-    type: ADD_QUESTION,
-    question,
-  };
-}
-
-export function handleAddQuestion(optionOneText, optionTwoText) {
-  return (dispatch, getState) => {
-    const { authedUser } = getState();
-
-    return saveQuestion({
-      optionOneText,
-      optionTwoText,
-      author: authedUser,
-    }).then((question) => {
-      dispatch(addQuestion(question));
-      dispatch(updateUserQuestions(question));
+export const receiveQuestions = () => async (dispatch) => {
+  try {
+    const res = await QuestionDataService.getAll();
+    dispatch({
+      type: RECEIVE_QUESTIONS,
+      payload: res.data,
     });
-  };
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-function updateQuestion({ authedUser, qid, answer }) {
-  return {
-    type: UPDATE_QUESTION,
-    authedUser,
-    qid,
-    answer,
-  };
-}
+// const createQuestion = (question) => {
+//   return {
+//     type: CREATE_QUESTION,
+//     payload: question,
+//   };
+// };
 
-export function handleUpdateQuestion(authedUser, qid, answer) {
-  return (dispatch) => {
-    dispatch(updateQuestion(authedUser, qid, answer));
-    dispatch(updateUserAnswers(authedUser, qid, answer));
+// export const handleCreateQuestion =
+//   (optionOneText, optionTwoText) => async (dispatch, getState) => {
+//     const { authedUser } = getState();
+//     try {
+//       const res = await QuestionDataService.create({
+//         optionOneText,
+//         optionTwoText,
+//         author: authedUser,
+//       });
+//       dispatch(createQuestion(res.data));
+//       return Promise.resolve(res.data);
+//     } catch (error) {
+//       return Promise.reject(error);
+//     }
+//   };
 
-    return saveQuestionAnswer(authedUser, qid, answer);
+export const createQuestion =
+  (optionOneText, optionTwoText) => async (dispatch, getState) => {
+    const { authedUser } = getState();
+    try {
+      const res = await QuestionDataService.create({
+        optionOneText,
+        optionTwoText,
+        author: authedUser,
+      });
+      dispatch({
+        type: CREATE_QUESTION,
+        payload: res.data,
+      });
+      return Promise.resolve(res.data);
+    } catch (error) {
+      return Promise.reject(error);
+    }
   };
-}
+
+export const updateQuestion = (id, data) => async (dispatch, getState) => {
+  const { authedUser } = getState();
+  const answer = data.answer;
+  try {
+    const res = await QuestionDataService.update(id, data);
+    const q = res.data;
+
+    dispatch({
+      type: UPDATE_QUESTION,
+      // payload: res.data
+      q,
+      authedUser,
+      answer,
+    });
+
+    return Promise.resolve(res.data);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+// export function receiveQuestions(questions) {
+//   return {
+//     type: RECEIVE_QUESTIONS,
+//     questions,
+//   };
+// }
+
+// export const updateQuestion = (authedUser, qid, answer) => async (dispatch) => {
+//   try {
+//     const res = await QuestionDataService.update(authedUser, qid, answer);
+//     dispatch({
+//       type: UPDATE_QUESTION,
+//       payload: res.data,
+//     });
+//     return Promise.resolve(res.data);
+//   } catch (error) {
+//     return Promise.reject(error);
+//   }
+// };
+
+// function updateQuestion({ authedUser, qid, answer }) {
+//   return {
+//     type: UPDATE_QUESTION,
+//     authedUser,
+//     qid,
+//     answer,
+//   };
+// }
+
+// export function handleUpdateQuestion(authedUser, qid, answer) {
+//   return (dispatch) => {
+//     dispatch(updateQuestion(authedUser, qid, answer));
+//     dispatch(updateUserAnswers(authedUser, qid, answer));
+
+//     return saveQuestionAnswer(authedUser, qid, answer);
+//   };
+// }
