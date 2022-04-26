1ddf3b3dde35ed8f389f5d2416bc31d1b4e47e7a
Hanna <hannaeberhoefer@yahoo.com>
2022-04-26 14:25:32 +0100

    feat: create role model

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,9 +1,19 @@
 const dbConfig = require("../config/db.config.js");
+
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
+
 db.questions = require("./question.model.js")(mongoose);
+db.user = require("./user.model");
+db.role = require("./role.model");
+db.ROLES = ["user", "admin"];
+
+// db.user = require("./user.model.js")(mongoose);
+// db.role = require("./role.model.js")(mongoose);
+// db.ROLES = ["user", "admin"];
+
 module.exports = db;
