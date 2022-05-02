5327b109047e8f1e75a68e8a08b6192ba117bdc7
Hanna <hannaeberhoefer@yahoo.com>
2022-05-02 10:38:38 +0100

    feat: avatars

---
+++ client/src/utils/avatars.js
@@ -0,0 +1,27 @@
+import avatar0 from "../images/avatar0.png";
+import avatar1 from "../images/avatar1.png";
+import avatar2 from "../images/avatar2.png";
+import avatar3 from "../images/avatar3.png";
+import avatar4 from "../images/avatar4.png";
+import avatar5 from "../images/avatar5.png";
+import avatar6 from "../images/avatar6.png";
+import avatar7 from "../images/avatar7.png";
+import avatar8 from "../images/avatar8.png";
+import avatar9 from "../images/avatar9.png";
+import avatar10 from "../images/avatar10.png";
+import avatar11 from "../images/avatar11.png";
+
+export const avatars = [
+  avatar0,
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+  avatar7,
+  avatar8,
+  avatar9,
+  avatar10,
+  avatar11,
+];
