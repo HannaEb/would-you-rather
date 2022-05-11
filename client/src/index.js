26f1a481ef7f94981b0b367c0d05bba8100e49db
Hanna <hannaeberhoefer@yahoo.com>
2022-05-11 16:25:16 +0100

    refactor: store

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "./App.css";
-import App from "./App";
-import reducer from "./reducers";
-import middleware from "./middleware";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-const store = createStore(reducer, composeWithDevTools(middleware));
 
 ReactDOM.render(
   <Provider store={store}>
