6fac87200c7e96f28ced095f2271288dc42475b4
Hanna <hannaeberhoefer@yahoo.com>
2022-05-13 17:15:44 +0100

    feat: verifyToken

--- client/src/services/question.service.js
+++ client/src/services/question.service.js
@@ -1,20 +1,21 @@
 import http from "../http-common";
+import authHeader from "./auth-header";
 
 class QuestionDataService {
   create = (data) => {
-    return http.post("/questions", data);
+    return http.post("/questions", data, { headers: authHeader() });
   };
   get = (id) => {
-    return http.get(`/questions/${id}`);
+    return http.get(`/questions/${id}`, { headers: authHeader() });
   };
   getAll = () => {
-    return http.get("/questions");
+    return http.get("/questions", { headers: authHeader() });
   };
   update = (id, data) => {
-    return http.put(`/questions/${id}`, { data });
+    return http.put(`/questions/${id}`, { data }, { headers: authHeader() });
   };
   delete = (id, data) => {
-    return http.delete(`/questions/${id}`, { data });
+    return http.delete(`/questions/${id}`, { data }, { headers: authHeader() });
   };
 }
 
--- server/app/routes/question.routes.js
+++ server/app/routes/question.routes.js
@@ -3,11 +3,24 @@ const questions = require("../controllers/question.controller.js");
 const router = require("express").Router();
 
 module.exports = (app) => {
-  router.post("/questions/", questions.create);
-  router.get("/questions/", questions.findAll);
-  router.get("/questions/:id", questions.findOne);
-  router.put("/questions/:id", questions.update);
-  router.delete("/questions/:id", [authJwt.isAdmin], questions.delete);
+  router.post("/questions/", [authJwt.verifyToken], questions.create);
+  router.get("/questions/", [authJwt.verifyToken], questions.findAll);
+  router.get("/questions/:id", [authJwt.verifyToken], questions.findOne);
+  router.put("/questions/:id", [authJwt.verifyToken], questions.update);
+  router.delete(
+    "/questions/:id",
+    [authJwt.verifyToken],
+    [authJwt.isAdmin],
+    questions.delete
+  );
+
+  app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
 
   app.use("/api", router);
 };
