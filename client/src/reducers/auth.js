d3d8bf29db899e4325d675d429c0fb106faf9afb
Hanna <hannaeberhoefer@yahoo.com>
2022-07-14 13:35:25 +0100

    refactor: immer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,3 +1,4 @@
+import produce from "immer";
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -14,33 +15,28 @@ const initialState = user
 const auth = (state = initialState, action) => {
   switch (action.type) {
     case REGISTER_SUCCESS:
-      return {
-        ...state,
-        isLoggedIn: false,
-      };
+      return produce(state, (draft) => {
+        draft.isLoggedIn = false;
+      });
     case REGISTER_FAIL:
-      return {
-        ...state,
-        isLoggedIn: false,
-      };
+      return produce(state, (draft) => {
+        draft.isLoggedIn = false;
+      });
     case LOGIN_SUCCESS:
-      return {
-        ...state,
-        isLoggedIn: true,
-        user: action.payload.user,
-      };
+      return produce(state, (draft) => {
+        draft.isLoggedIn = true;
+        draft.user = action.payload.user;
+      });
     case LOGIN_FAIL:
-      return {
-        ...state,
-        isLoggedIn: false,
-        user: null,
-      };
+      return produce(state, (draft) => {
+        draft.isLoggedIn = false;
+        draft.user = null;
+      });
     case LOGOUT:
-      return {
-        ...state,
-        isLoggedIn: false,
-        user: null,
-      };
+      return produce(state, (draft) => {
+        draft.isLoggedIn = false;
+        draft.user = null;
+      });
     default:
       return state;
   }
