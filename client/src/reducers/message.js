b2b4a0547b120e5d39625642f7f63aa9dd5aa737
Hanna <hannaeberhoefer@yahoo.com>
2022-07-14 13:35:46 +0100

    refactor: immer

--- client/src/reducers/message.js
+++ client/src/reducers/message.js
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { SET_MESSAGE, CLEAR_MESSAGE } from "../actions/message";
 
 const initialState = {};
@@ -5,9 +6,13 @@ const initialState = {};
 const message = (state = initialState, action) => {
   switch (action.type) {
     case SET_MESSAGE:
-      return { message: action.payload };
+      return produce(state, (draft) => {
+        draft.message = action.payload;
+      });
     case CLEAR_MESSAGE:
-      return { message: "" };
+      return produce(state, (draft) => {
+        draft.message = "";
+      });
     default:
       return state;
   }
