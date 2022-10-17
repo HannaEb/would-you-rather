1bc28fa52218193730704a4fedc50ba72cd30cdf
Hanna <hannaeberhoefer@yahoo.com>
2022-10-17 13:22:18 +0100

    refactor: schemas

--- server/app/models/question.model.js
+++ server/app/models/question.model.js
@@ -1,28 +1,30 @@
-module.exports = (mongoose) => {
-  const schema = mongoose.Schema({
-    timestamp: {
-      type: Number,
-      default: Date.now,
-    },
-    author: {
-      type: String,
-      required: true,
-    },
-    optionOne: {
-      votes: { type: Array },
-      text: { type: String, required: true },
-    },
-    optionTwo: {
-      votes: { type: Array },
-      text: { type: String, required: true },
-    },
-  });
+const mongoose = require("mongoose");
 
-  schema.method("toJSON", function () {
-    const { __v, _id, ...object } = this.toObject();
-    object.id = _id;
-    return object;
-  });
-  const Question = mongoose.model("question", schema);
-  return Question;
-};
+const questionSchema = new mongoose.Schema({
+  timestamp: {
+    type: Number,
+    default: Date.now,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  optionOne: {
+    votes: { type: Array },
+    text: { type: String, required: true },
+  },
+  optionTwo: {
+    votes: { type: Array },
+    text: { type: String, required: true },
+  },
+});
+
+schema.method("toJSON", function () {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+const Question = mongoose.model("Question", questionSchema);
+
+module.exports = Question;
--- server/app/models/user.model.js
+++ server/app/models/user.model.js
@@ -1,36 +1,38 @@
-module.exports = (mongoose) => {
-  const schema = mongoose.Schema({
-    username: {
-      type: String,
-      required: true,
-    },
-    avatar: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    answers: {
-      type: Array,
-    },
-    questions: {
-      type: Array,
-    },
-    roles: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Role",
-      },
-    ],
-  });
-  schema.method("toJSON", function () {
-    const { __v, _id, ...object } = this.toObject();
-    object.id = _id;
-    return object;
-  });
+const mongoose = require("mongoose");
 
-  const User = mongoose.model("user", schema);
-  return User;
-};
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  answers: {
+    type: Array,
+  },
+  questions: {
+    type: Array,
+  },
+  roles: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Role",
+    },
+  ],
+});
+
+schema.method("toJSON", function () {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
