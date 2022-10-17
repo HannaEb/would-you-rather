14ec29968c75e8d9f3d11161b6313f82bc103cbd
Hanna <hannaeberhoefer@yahoo.com>
2022-10-17 16:13:53 +0100

    refactor: restrictTo

--- server/app/routes/question.routes.js
+++ server/app/routes/question.routes.js
@@ -4,8 +4,6 @@ const auth = require("../controllers/auth.controller");
 
 const router = express.Router();
 
-const { authJwt } = require("../middleware");
-
 router
   .route("/")
   .get(auth.verifyToken, questions.getAllQuestions)
@@ -15,6 +13,6 @@ router
   .route("/:id")
   .get(auth.verifyToken, questions.getQuestion)
   .put(auth.verifyToken, questions.updateQuestion)
-  .delete(auth.verifyToken, authJwt.isAdmin, questions.deleteQuestion);
+  .delete(auth.verifyToken, auth.restrictTo("admin"), questions.deleteQuestion);
 
 module.exports = router;
