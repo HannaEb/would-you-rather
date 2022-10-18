39ab25fa195a846dd57aed7f02cbcecb1d052139
Hanna <hannaeberhoefer@yahoo.com>
2022-10-18 14:58:22 +0100

    refactor: register

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,36 +8,52 @@ export const LOGOUT = "LOGOUT";
 export const SET_MESSAGE = "SET_MESSAGE";
 export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
 
-export const register = (username, avatar, password) => (dispatch) => {
-  return AuthDataService.register(username, avatar, password).then(
-    (response) => {
-      dispatch({
-        type: REGISTER_SUCCESS,
-        payload: response.data,
-      });
-      dispatch({
-        type: CLEAR_MESSAGE,
-      });
-      return Promise.resolve();
-    },
-    (error) => {
-      const message =
-        (error.response &&
-          error.response.data &&
-          error.response.data.message) ||
-        error.message ||
-        error.toString();
-      dispatch({
-        type: REGISTER_FAIL,
-      });
-      dispatch({
-        type: SET_MESSAGE,
-        payload: message,
-      });
-      return Promise.reject();
-    }
-  );
+export const register = (username, avatar, password) => async (dispatch) => {
+  try {
+    const res = await AuthDataService.register(username, avatar, password);
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+    return Promise.resolve(res.data);
+  } catch (error) {
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+    return Promise.reject(error);
+  }
 };
+
+// export const register = (username, avatar, password) => (dispatch) => {
+//   return AuthDataService.register(username, avatar, password).then(
+//     (response) => {
+//       dispatch({
+//         type: REGISTER_SUCCESS,
+//         payload: response.data,
+//       });
+//       dispatch({
+//         type: CLEAR_MESSAGE,
+//       });
+//       return Promise.resolve();
+//     },
+//     (error) => {
+//       const message =
+//         (error.response &&
+//           error.response.data &&
+//           error.response.data.message) ||
+//         error.message ||
+//         error.toString();
+//       dispatch({
+//         type: REGISTER_FAIL,
+//       });
+//       dispatch({
+//         type: SET_MESSAGE,
+//         payload: message,
+//       });
+//       return Promise.reject();
+//     }
+//   );
+// };
 
 export const login = (username, password) => (dispatch) => {
   return AuthDataService.login(username, password).then(
