162c180cbeea0d58625ef8340c5851e3abbc0736
Hanna <hannaeberhoefer@yahoo.com>
2022-10-27 16:26:29 +0100

    feat: virtuals

--- server/app/models/question.model.js
+++ server/app/models/question.model.js
@@ -1,38 +1,38 @@
 const mongoose = require("mongoose");
 
-const questionSchema = new mongoose.Schema({
-  timestamp: {
-    type: Number,
-    default: Date.now,
-  },
-  author: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-    required: [true, "Question must belong to an author"],
-  },
-  optionOne: {
-    votes: { type: Array },
-    text: {
-      type: String,
-      required: [true, "Please enter first question"],
-      trim: true,
+const questionSchema = new mongoose.Schema(
+  {
+    timestamp: {
+      type: Number,
+      default: Date.now,
+    },
+    author: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Question must belong to an author"],
+    },
+    optionOne: {
+      votes: { type: Array },
+      text: {
+        type: String,
+        required: [true, "Please enter first question"],
+        trim: true,
+      },
+    },
+    optionTwo: {
+      votes: { type: Array },
+      text: {
+        type: String,
+        required: [true, "Please enter second question"],
+        trim: true,
+      },
     },
   },
-  optionTwo: {
-    votes: { type: Array },
-    text: {
-      type: String,
-      required: [true, "Please enter second question"],
-      trim: true,
-    },
-  },
-});
-
-questionSchema.method("toJSON", function () {
-  const { __v, _id, ...object } = this.toObject();
-  object.id = _id;
-  return object;
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 questionSchema.pre(/^find/, function (next) {
   this.populate({
--- server/app/models/user.model.js
+++ server/app/models/user.model.js
@@ -1,40 +1,40 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Please enter a username"],
-    unique: [true, "Username unavailable"],
-    maxlength: 15,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please enter a username"],
+      unique: [true, "Username unavailable"],
+      maxlength: 15,
+    },
+    avatar: {
+      type: String,
+      required: [true, "Please select an avatar"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+      select: false,
+    },
+    answers: {
+      type: Array,
+    },
+    questions: {
+      type: Array,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-  avatar: {
-    type: String,
-    required: [true, "Please select an avatar"],
-  },
-  password: {
-    type: String,
-    required: [true, "Please enter a password"],
-    select: false,
-  },
-  answers: {
-    type: Array,
-  },
-  questions: {
-    type: Array,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-});
-
-userSchema.method("toJSON", function () {
-  const { __v, _id, ...object } = this.toObject();
-  object.id = _id;
-  return object;
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 // Hash password asynchronously with cost of 10 (security level)
 userSchema.pre("save", async function (next) {
