238b09254b899c947520ac0ba3b5ae0ad81a4e18
Hanna <hannaeberhoefer@yahoo.com>
2022-11-07 15:12:58 +0000

    feat: AppError

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,9 +1,17 @@
 const authRouter = require("./auth.routes");
 const userRouter = require("./user.routes");
 const questionRouter = require("./question.routes");
+const AppError = require("../utils/appError");
+const errorHandler = require("../controllers/error.controller");
 
 module.exports = (app) => {
   app.use("/api/auth", authRouter);
   app.use("/api/users", userRouter);
   app.use("/api/questions", questionRouter);
+
+  app.all("*", (req, res, next) => {
+    next(new AppError(`${req.originalUrl} not found`, 404));
+  });
+
+  app.use(errorHandler);
 };
