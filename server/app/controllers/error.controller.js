13dd9a617a1f674a2dcfe9b2443ecb1a68651692
Hanna <hannaeberhoefer@yahoo.com>
2022-11-08 13:43:21 +0000

    feat: invalid id error

--- server/app/controllers/error.controller.js
+++ server/app/controllers/error.controller.js
@@ -1,3 +1,10 @@
+const AppError = require("./../utils/appError");
+
+const handleCastError = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
@@ -10,6 +17,8 @@ module.exports = (err, req, res, next) => {
       stack: err.stack,
     });
   } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    if (error.name === "CastError") error = handleCastError(error);
     if (err.isOperational) {
       res.status(err.statusCode).json({
         status: err.status,
