25c9265a858a62e06b126f223ee91c8bf8230b02
Hanna <hannaeberhoefer@yahoo.com>
2022-11-14 10:13:45 +0000

    refactor: rm receiveQuestions

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -3,7 +3,6 @@ import { useSelector, useDispatch } from "react-redux";
 import { Row, Col, List } from "reactstrap";
 import Leader from "./Leader";
 import { receiveUsers } from "../actions/users";
-import { receiveQuestions } from "../actions/questions";
 
 const Leaderboard = () => {
   const users = useSelector((state) => state.users);
@@ -11,7 +10,6 @@ const Leaderboard = () => {
 
   useEffect(() => {
     dispatch(receiveUsers());
-    dispatch(receiveQuestions());
   }, [dispatch]);
 
   const sortedUsers = Object.values(users)
