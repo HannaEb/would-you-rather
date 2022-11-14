94b8bd7947b90c004008ec20573e587ead1c071f
Hanna <hannaeberhoefer@yahoo.com>
2022-11-14 15:36:04 +0000

    refactor: store

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,23 @@
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import reducer from "./reducers";
-import middleware from "./middleware";
+import { configureStore } from "@reduxjs/toolkit";
+import { loadingBarReducer } from "react-redux-loading-bar";
+import auth from "./reducers/auth";
+import users from "./reducers/users";
+import questions from "./reducers/questions";
+import message from "./reducers/message";
+import logger from "./middleware/logger";
+import { apiSlice } from "./features/api/apiSlice";
 
-const store = createStore(reducer, composeWithDevTools(middleware));
+const store = configureStore({
+  reducer: {
+    auth,
+    users,
+    questions,
+    message,
+    loadingBar: loadingBarReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware, logger),
+});
 
 export default store;
