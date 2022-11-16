5a743ee5618322d40374949c529d9fb6ecf552de
Hanna <hannaeberhoefer@yahoo.com>
2022-11-16 12:18:35 +0000

    refactor: rm updateUserQuestions

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -1,15 +1,4 @@
-export const UPDATE_USER_QUESTIONS = "UPDATE_USER_QUESTIONS";
 export const UPDATE_USER_ANSWERS = "UPDATE_USER_ANSWERS";
-
-export const updateUserQuestions = (id, author) => {
-  return {
-    type: UPDATE_USER_QUESTIONS,
-    payload: {
-      id,
-      author,
-    },
-  };
-};
 
 export const updateUserAnswers = (id, authedUser, answer) => {
   return {
--- client/src/reducers/users.js
+++ client/src/reducers/users.js
@@ -1,16 +1,10 @@
 import produce from "immer";
-import { UPDATE_USER_QUESTIONS, UPDATE_USER_ANSWERS } from "../actions/users";
+import { UPDATE_USER_ANSWERS } from "../actions/users";
 
 const initialState = {};
 
 const users = (state = initialState, action) => {
   switch (action.type) {
-    case UPDATE_USER_QUESTIONS:
-      return produce(state, (draft) => {
-        draft[action.payload.author].questions = draft[
-          action.payload.author
-        ].questions.concat([action.payload.id]);
-      });
     case UPDATE_USER_ANSWERS:
       return produce(state, (draft) => {
         draft[action.payload.authedUser].answers = draft[
