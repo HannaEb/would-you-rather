dd8a5c9af7b269371530324d37730b5a34b258d1
Hanna <hannaeberhoefer@yahoo.com>
2022-11-18 16:22:25 +0000

    feat: rm LoadingBar

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { loadingBarReducer } from "react-redux-loading-bar";
 import authReducer from "./features/auth/authSlice";
 import message from "./reducers/message";
 import logger from "./middleware/logger";
@@ -9,7 +8,6 @@ const store = configureStore({
   reducer: {
     auth: authReducer,
     message,
-    loadingBar: loadingBarReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
